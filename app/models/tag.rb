b7e1e534b052704d9de49b3ddf613eefb079e7f0
Mitch Joa <mitch.joa@yello.co>
2018-01-30 07:36:25 -0600

    Add uniqueness constraint to tag joins

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,9 @@
 class Tag < ApplicationRecord
   belongs_to :user
-  belongs_to :taggable, polymorphic: true
+  belongs_to :taggable, polymorphic: true, optional: true
 
   has_many :tag_joins
   has_many :bookmarks, through: :tag_joins, source: :taggable, source_type: 'Bookmark'
+
+  validates_presence_of :name, :user_id
 end
