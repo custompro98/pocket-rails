8c60de789e01fb03c9751391e3d6e26058135577
custompro98 <mitchjoa@gmail.com>
2018-02-03 00:30:34 -0600

    Add API docs through dox and publish to apiary

--- spec/requests/v1/get_bookmark_spec.rb
+++ spec/requests/v1/get_bookmark_spec.rb
@@ -1,41 +1,47 @@
 require 'rails_helper'
 
-describe 'GET /bookmarks/:id', type: :request do
-  let(:owner) { create(:user) }
+describe 'Bookmarks', type: :request do
+  include ::Docs::Bookmarks::Api
 
-  before { get v1_bookmark_path(bookmark), headers: headers(owner) }
+  describe 'GET /bookmarks/:id' do
+    include ::Docs::Bookmarks::Show
 
-  context 'when the bookmark exists and is owned by current user' do
-    let!(:bookmark) { create(:bookmark, user_id: owner.id) }
-    let(:status) { :ok }
+    let(:owner) { create(:user) }
 
-    it_behaves_like 'a successful request'
+    before { get v1_bookmark_path(bookmark), headers: headers(owner) }
 
-    it 'returns the todo' do
-      expect(json).not_to be_empty
-      expect(json[:id]).to eq bookmark.id
+    context 'when the bookmark exists and is owned by current user' do
+      let!(:bookmark) { create(:bookmark, user_id: owner.id) }
+      let(:status) { :ok }
+
+      it_behaves_like 'a successful request'
+
+      it 'returns the bookmark', dox: true do
+        expect(json).not_to be_empty
+        expect(json[:id]).to eq bookmark.id
+      end
     end
-  end
 
-  context 'when the bookmark exists and is not owned by current user' do
-    let!(:bookmark) { create(:bookmark) }
-    let(:status) { :not_found }
+    context 'when the bookmark exists and is not owned by current user' do
+      let!(:bookmark) { create(:bookmark) }
+      let(:status) { :not_found }
 
-    it_behaves_like 'an unsuccessful request'
+      it_behaves_like 'an unsuccessful request'
 
-    it 'returns a not found message' do
-      expect(json[:message]).to eq 'Bookmark not found'
+      it 'returns a not found message', dox: true do
+        expect(json[:message]).to eq 'Bookmark not found'
+      end
     end
-  end
 
-  context 'when the bookmark does not exist' do
-    let(:bookmark) { OpenStruct.new(id: 1) }
-    let(:status) { :not_found }
+    context 'when the bookmark does not exist' do
+      let(:bookmark) { OpenStruct.new(id: 1) }
+      let(:status) { :not_found }
 
-    it_behaves_like 'an unsuccessful request'
+      it_behaves_like 'an unsuccessful request'
 
-    it 'returns a not found message' do
-      expect(json[:message]).to eq 'Bookmark not found'
+      it 'returns a not found message', dox: true do
+        expect(json[:message]).to eq 'Bookmark not found'
+      end
     end
   end
 end
