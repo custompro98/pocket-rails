1383bec4764c419db750cd93c6425bcf4bcd2d79
Mitch Joa <mitch.joa@yello.co>
2018-02-24 16:12:48 -0600

    Add graphql mutation for delete tag

---
+++ spec/requests/v1/delete_tag_graphql_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe 'Tags', type: :request do
+  describe 'POST /graphql - Tag Mutation Delete', type: :request do
+    let(:owner) { create(:user) }
+    let(:query) do
+      { "query":
+          "mutation deleteTag{
+            deleteTag(input: { id: #{tag.id} })
+            { tag { id } }
+          }" }
+    end
+
+    before { post v1_graphql_path, params: query.to_json, headers: headers(owner) }
+
+    context 'the tag to delete exists and is owned by current user' do
+      let(:tag) { create(:tag, user_id: owner.id) }
+
+      it_behaves_like 'a successful request'
+
+      it 'deletes the tag' do
+        expect{ ::Tag.find(tag.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'the tag to delete exists and is not owned by current user' do
+      let(:tag) { create(:tag) }
+
+      it_behaves_like 'a successful request'
+
+      it 'does not delete the tag' do
+        expect(::Tag.find(tag.id)).to eq tag
+      end
+
+      it 'returns an error message' do
+        expect(json[:error]).to eq 'Tag not found'
+      end
+    end
+
+    context 'the tag to delete does not exist' do
+      let(:tag) { OpenStruct.new(id: 1) }
+
+      it_behaves_like 'a successful request'
+
+      it 'returns an error message' do
+        expect(json[:error]).to eq 'Tag not found'
+      end
+    end
+  end
+end
