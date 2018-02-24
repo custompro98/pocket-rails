96f558efb36a709602377a34d736d7a18fe48429
custompro98 <mitchjoa@gmail.com>
2018-02-24 16:00:38 -0600

    Add graphql mutations for delete bookmark

---
+++ spec/requests/v1/delete_bookmark_graphql_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe 'Bookmarks', type: :request do
+  describe 'POST /graphql - Bookmark Mutation Delete', type: :request do
+    let(:owner) { create(:user) }
+    let(:query) do
+      { "query":
+          "mutation deleteBookmark{
+            deleteBookmark(input: { id: #{bookmark.id} })
+            { bookmark { id } }
+          }" }
+    end
+
+    before { post v1_graphql_path, params: query.to_json, headers: headers(owner) }
+
+    context 'the bookmark to delete exists and is owned by current user' do
+      let(:bookmark) { create(:bookmark, user_id: owner.id) }
+
+      it_behaves_like 'a successful request'
+
+      it 'deletes the bookmark' do
+        expect{ ::Bookmark.find(bookmark.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'the bookmark to delete exists and is not owned by current user' do
+      let(:bookmark) { create(:bookmark) }
+
+      it_behaves_like 'a successful request'
+
+      it 'does not delete the bookmark' do
+        expect(::Bookmark.find(bookmark.id)).to eq bookmark
+      end
+
+      it 'returns an error message' do
+        expect(json[:error]).to eq 'Bookmark not found'
+      end
+    end
+
+    context 'the bookmark to delete does not exist' do
+      let(:bookmark) { OpenStruct.new(id: 1) }
+
+      it_behaves_like 'a successful request'
+
+      it 'returns an error message' do
+        expect(json[:error]).to eq 'Bookmark not found'
+      end
+    end
+  end
+end
