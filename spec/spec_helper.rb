c878c582263ce0b30464986faec1d39890443078
custompro98 <mitchjoa@gmail.com>
2018-02-23 21:03:44 -0600

    Run coverage for the whole suite, not just api docs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
-require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter do |source_file|
-    source_file.lines.count < 5
+if ENV['RUN_COVERAGE']
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    add_filter do |source_file|
+      source_file.lines.count < 5
+    end
   end
 end
 
